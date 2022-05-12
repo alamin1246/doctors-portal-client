b42b32afd8f746e6bef8aa621b51ff11dcfa7f40
alamin1246 <hossainalamin1246@gmail.com>
2022-05-12 12:50:57 +0600

    home page work successfully

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Footer from '../Shared/Footer/Footer';
+import Banner from './Banner';
+import Contact from './Contact';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Section from './Section';
+import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
-        <div>
-            This is Home
+        <div className='px-12'>
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <Section></Section>
+            <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
